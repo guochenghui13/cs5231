56a4bca17640b997abeeea27a0d95f05826fceb3
resdust winter <resdust_li@163.com>
2022-11-26 19:05:10 +0800

    modify log item class, add print function

--- scripts/parse_rules.py
+++ scripts/parse_rules.py
@@ -4,18 +4,28 @@
 import re
 
 class LogItem:
-  def __init__(self, tag, category, description = "", log = "") -> None:
-    self.tag = tag
-    self.category = category
-    self.description = description
+  def __init__(self, rule_type = None, log = []) -> None:
+    self.ruletype = rule_type
+    self.log = log
+  
+  def addLog(self, log):
     self.log = log
 
+  def __str__(self) -> str:
+    return self.ruletype.__str__() + '\n' + 'log: ' + self.log.__str__()
+
 class RuleType:
-  def __init__(self, category, description = "", tag = "") -> None:
+  def __init__(self, category, tag = "", description = "") -> None:
     self.category = category
     self.description = description
     self.tag = tag
-    self.logs = []
+  
+  def __str__(self) -> str:
+    str = ""
+    str += "category: " + self.category + "\n"
+    str += "description: " + self.description + "\n"
+    str += "tag: " + self.tag + "\n"
+    return str
 
 class CatchRules:
   def __init__(self) -> None:
@@ -28,13 +38,24 @@ class CatchRules:
     except:
       self.rules[cate] = [rule_type]
   
+  def search_rule(self, tagname):
+    tag_rules = []
+    for cate, rules in self.rules.items():
+      for rule in rules:
+        if rule.tag == tagname:
+          tag_rules.append(rule)
+    if (tag_rules == []):
+      print("no such tag!", tagname)
+    return tag_rules
+
   def __str__(self) -> str:
     rules_str = ""
     for cate, rules in self.rules.items():
       rules_str += cate + ':\n'
       for rule in rules:
-        rules_str += '\tdescription: '+rule.description+'\n'
-        rules_str += '\ttag: '+rule.tag+'\n'
+        rules_str += rule.__str__()
+        # rules_str += '\tdescription: '+rule.description+'\n'
+        # rules_str += '\ttag: '+rule.tag+'\n'
     return rules_str
 
 def parse_rules(rule_file, log_types):
@@ -55,9 +76,19 @@ def parse_rules(rule_file, log_types):
         despt = " ".join(line.split()[1:])
       elif (tag):
         tag = tag[0]
-        rule = RuleType(cate, despt, tag)
+        rule = RuleType(cate, tag, despt)
         log_types.add_rule(rule)
 
-log_types = CatchRules()
-parse_rules("../rules/example.rules", log_types)
-print(log_types)
+def main():
+  log_types = CatchRules()
+  parse_rules("../rules/example.rules", log_types)
+  # print(log_types)
+  return log_types
+
+def test(log_types):
+  for rule in log_types.search_rule(tagname = 'auditconfig'):
+    print(rule)
+
+if __name__ == "__main__":
+  log_types = main()
+  test(log_types)

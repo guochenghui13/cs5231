b39c57bf166de3d276a7744ad3033f3daa717aca
Suri Zhang <surizhang1224@gmail.com>
2022-11-27 17:20:38 +0800

    add placeholder to parse -w logs

--- scripts/parse_logs.py
+++ scripts/parse_logs.py
@@ -43,83 +43,88 @@ def parse(filename, print_style = ""):
         syscall = ''
         # use tag to find matching rule type
         rule_type = CatchRules().search_rule(tag)[0]
-        if tag == 'sys_access':
-            try:
-                syscall = data['auditd']['data']['syscall']
-                process_name = data['process']['name']
-                process_executable = data['process']['executable']
-                pid = data['process']['pid']
-            except KeyError as e:
-                continue
-            if syscall == 'read' or syscall == 'write' or syscall == 'writev':
+        if rule_type.type == 'syscall':
+            if tag == 'sys_access':
                 try:
-                    a0 = data['auditd']['data']['a0']
-                    accessed_file = fd_map.get(a0, "unknown")
+                    syscall = data['auditd']['data']['syscall']
+                    process_name = data['process']['name']
+                    process_executable = data['process']['executable']
+                    pid = data['process']['pid']
                 except KeyError as e:
                     continue
-            elif syscall == 'open' or syscall == 'openat':
-                try:
-                    file_path = data['file']['path']
-                    exit = data['auditd']['data']['exit']
-                except KeyError as e:
-                    continue
-                fd_map[exit] = file_path
-                accessed_file = file_path
+                if syscall == 'read' or syscall == 'write' or syscall == 'writev':
+                    try:
+                        a0 = data['auditd']['data']['a0']
+                        accessed_file = fd_map.get(a0, "unknown")
+                    except KeyError as e:
+                        continue
+                elif syscall == 'open' or syscall == 'openat':
+                    try:
+                        file_path = data['file']['path']
+                        exit = data['auditd']['data']['exit']
+                    except KeyError as e:
+                        continue
+                    fd_map[exit] = file_path
+                    accessed_file = file_path
 
-            if (accessed_file.startswith("/home/student") or "program" in process_executable):
-                output.write(log)
-                log_content = (timestamp, "syscall="+syscall, "executable="+process_executable, "pid="+str(pid), "accessed_file="+accessed_file)
-                events[seq] = LogItem(rule_type = rule_type, log=log_content)
-        if tag == 'sys_exe':
-            try:
-                syscall = data['auditd']['data']['syscall']
-                process_name = data['process']['name']
-                process_executable = data['process']['executable']
-                pid = data['process']['pid']
-            except KeyError as e:
-                continue
-            if syscall == 'execve':
-                try:
-                    process_args = data['process']['args']
-                except KeyError as e:
-                    continue
-                output.write(log)
-                log_content = (timestamp, "syscall="+syscall, "executable="+process_executable,"pid="+str(pid), "args="+str(process_args))
-                events[seq] = LogItem(rule_type = rule_type, log=log_content)
-        
-        if tag == 'sys_curl' or tag == 'power_abuse':
-            try:
-                syscall = data['auditd']['data']['syscall']
-                process_executable = data['process']['executable']
-                pid = data['process']['pid']
-            except KeyError as e:
-                continue
-
-            if syscall == 'openat' or syscall == 'open':
-                try:
-                    paths = data['auditd']['paths']
-                    file_path = data['file']['path']
-                except KeyError as e:
-                    continue
-
-                if len(paths) > 1:
-                    name_type = paths[1]['nametype']
-                    name = data['auditd']['paths'][1]['name']
-                
-                if name_type == 'CREATE':
+                if (accessed_file.startswith("/home/student") or "program" in process_executable):
                     output.write(log)
-                    log_content = (timestamp, "syscall="+syscall, "executable="+process_executable, "pid="+str(pid), "accessed_file="+file_path, "name_type="+name_type, "name="+name)
+                    log_content = (timestamp, "syscall="+syscall, "executable="+process_executable, "pid="+str(pid), "accessed_file="+accessed_file)
                     events[seq] = LogItem(rule_type = rule_type, log=log_content)
-            if syscall == 'connect':
+            if tag == 'sys_exe':
                 try:
-                    dest = data['destination']['path']
-                    socket = data['auditd']['data']['socket']
-                    result = data['auditd']['result']
+                    syscall = data['auditd']['data']['syscall']
+                    process_name = data['process']['name']
+                    process_executable = data['process']['executable']
+                    pid = data['process']['pid']
                 except KeyError as e:
                     continue
-                output.write(log)
-                log_content = (timestamp, "syscall="+syscall, "executable="+process_executable, "pid="+str(pid), "destination="+dest, "socket="+str(socket), "result="+result)
-                events[seq] = LogItem(rule_type = rule_type, log=log_content)
+                if syscall == 'execve':
+                    try:
+                        process_args = data['process']['args']
+                    except KeyError as e:
+                        continue
+                    output.write(log)
+                    log_content = (timestamp, "syscall="+syscall, "executable="+process_executable,"pid="+str(pid), "args="+str(process_args))
+                    events[seq] = LogItem(rule_type = rule_type, log=log_content)
+            
+            if tag == 'sys_curl' or tag == 'power_abuse':
+                try:
+                    syscall = data['auditd']['data']['syscall']
+                    process_executable = data['process']['executable']
+                    pid = data['process']['pid']
+                except KeyError as e:
+                    continue
+
+                if syscall == 'openat' or syscall == 'open':
+                    try:
+                        paths = data['auditd']['paths']
+                        file_path = data['file']['path']
+                    except KeyError as e:
+                        continue
+
+                    if len(paths) > 1:
+                        name_type = paths[1]['nametype']
+                        name = data['auditd']['paths'][1]['name']
+                    
+                    if name_type == 'CREATE':
+                        output.write(log)
+                        log_content = (timestamp, "syscall="+syscall, "executable="+process_executable, "pid="+str(pid), "accessed_file="+file_path, "name_type="+name_type, "name="+name)
+                        events[seq] = LogItem(rule_type = rule_type, log=log_content)
+                if syscall == 'connect':
+                    try:
+                        dest = data['destination']['path']
+                        socket = data['auditd']['data']['socket']
+                        result = data['auditd']['result']
+                    except KeyError as e:
+                        continue
+                    output.write(log)
+                    log_content = (timestamp, "syscall="+syscall, "executable="+process_executable, "pid="+str(pid), "destination="+dest, "socket="+str(socket), "result="+result)
+                    events[seq] = LogItem(rule_type = rule_type, log=log_content)
+            
+        elif rule_type.type == "file":
+            # parse -w logs here ...
+            continue
 
     od = collections.OrderedDict(sorted(events.items()))
     
